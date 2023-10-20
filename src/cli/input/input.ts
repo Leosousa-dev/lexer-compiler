57a3cc8733c74f012521be4fdcf5f04ceaabde29
leonardo <leonardocasa790@gmail.com>
2023-10-20 19:55:36 -0300

    chore: add erro mensage

--- src/cli/input/input.ts
+++ src/cli/input/input.ts
@@ -1,8 +1,12 @@
+import chalk from "chalk";
+
 export default function getPathFile(): string{
     const args = process.argv.slice(2);
 
     if(args.length === 0){
-        console.error("⛔ Nenhum parâmetro fornecido.\n\nPor favor informe o nome do arquivo que deseja processar")
+        console.error("⛔ Nenhum parâmetro fornecido.\n")
+        console.log(`Por favor informe o nome do arquivo que deseja processar.\n\n`)
+        console.log(`${chalk.gray("Ex: test.lang\n\n")}`)
         process.exit(1);
     }
 
