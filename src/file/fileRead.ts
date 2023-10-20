c4706986a38d81b5ac7114f67df2a20557241b9e
leonardo <leonardocasa790@gmail.com>
2023-10-20 15:13:19 -0300

    chore: get filepath with getPathFile()

--- src/file/fileRead.ts
+++ src/file/fileRead.ts
@@ -1,9 +1,10 @@
 import path from "node:path";
 import fs from "node:fs";
+import getPathFile from "../cli/input/input";
 
-const fileName = "tasks.md"
+const fileName = getPathFile()  // <- get param[pathFile]
 
-const currentDir = process.cwd()
+const currentDir = process.cwd() // <- get current path
 
 const fullPath = path.join(currentDir, fileName);
 
