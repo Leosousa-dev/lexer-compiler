c53dd478d5308d470496c2cc9de93b22b227f61c
leonardo <leonardocasa790@gmail.com>
2023-10-20 19:14:28 -0300

    chore: return file to lexer

--- src/file/fileRead.ts
+++ src/file/fileRead.ts
@@ -2,14 +2,12 @@ import path from "node:path";
 import fs from "node:fs";
 import getPathFile from "../cli/input/input";
 
-const fileName = getPathFile()  // <- get param[pathFile]
 
-const currentDir = process.cwd() // <- get current path
+export default function read(){
+    const fileName = getPathFile()  // <- get param[pathFile]
+    const currentDir = process.cwd() // <- get current path
+    const fullPath = path.join(currentDir, fileName);
+    const source = fs.readFileSync(fullPath, 'utf-8')
 
-const fullPath = path.join(currentDir, fileName);
-
-
-const argv = fs.readFileSync(fullPath, 'utf-8')
-
-
-console.log("test:", argv);
+    return source;
+}
