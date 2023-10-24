577f7ed353b5c4e2ed678f506c9eeca22714ce9d
leonardo <leonardocasa790@gmail.com>
2023-10-24 16:03:30 -0300

    chore: test scan

--- src/lexer.ts
+++ src/lexer.ts
@@ -4,20 +4,23 @@ function lexer(){
     const code = read()
     let index: number = 0;
     let char = "";
+    let testArry: string[] = [];
     const skipChar = new Set([" ", "\n", "\t"]);
 
     while(code.length > index){
         let currentChar = code[index];
         
         if(!skipChar.has(currentChar)){
+            char += currentChar
             console.log(currentChar)
         }
-        else{
-            console.log("empty space")
+        else if(char !== ""){
+            testArry.push(char)
+            char = ""
         }
         index++
     }
-
+    console.log(testArry)
 }
 
 lexer()
