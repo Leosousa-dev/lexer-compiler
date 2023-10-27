2fad132a3f784b2c0730dd1652b556f2d6b2a243
leonardo <leonardocasa790@gmail.com>
2023-10-27 10:29:41 -0300

    chore: refac lexer and add paser()

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,26 +1,11 @@
 import read from "./file/fileRead"
+import paser from "./processing/process";
 
 function lexer(){
     const code = read()
-    let index: number = 0;
-    let char = "";
-    let testArry: string[] = [];
-    const skipChar = new Set([" ", "\n", "\t"]);
 
-    while(code.length > index){
-        let currentChar = code[index];
-        
-        if(!skipChar.has(currentChar)){
-            char += currentChar
-            console.log(currentChar)
-        }
-        else if(char !== ""){
-            testArry.push(char)
-            char = ""
-        }
-        index++
-    }
-    console.log(testArry)
+    paser(code)
+
 }
 
 lexer()
