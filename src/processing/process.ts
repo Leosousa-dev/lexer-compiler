f92416275fe6436f504ab9f4ba603760166b7f97
leonardo <leonardocasa790@gmail.com>
2023-10-27 11:08:56 -0300

    chore: refact paser

--- src/processing/process.ts
+++ src/processing/process.ts
@@ -3,30 +3,15 @@ type Tokens = string
 
 export default function paser(code: string): Tokens[]{
     let tokens: Tokens[] = [];
-    let char: string = "";
     let index: number = 0;
-    const skipChar = new Set([" ", "\n", "\t", "\r",]);
 
-
-    while(code.length > index){
-        let currentToken = code[index];
-        
-        if(!skipChar.has(currentToken)){
-            char += currentToken;
-
-        }else if(char !== ""){
-            tokens.push(char)
-            char = ""
-
-        }
+    const source = code.split(" ")
+    source.push("eof")
+    while(source.length > index){
+                
 
         index++
     }
-    if (char !== "") {
-        tokens.push(char);
-    }
-    console.log(tokens)
-
 
     return tokens
 }
