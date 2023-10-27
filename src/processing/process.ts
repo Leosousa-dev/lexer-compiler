d0fb4ad5928cf89097d74c0ea18da989b48d6eea
leonardo <leonardocasa790@gmail.com>
2023-10-27 10:47:48 -0300

    chore: create a paser function

--- src/processing/process.ts
+++ src/processing/process.ts
@@ -1,22 +1,31 @@
 
-type Tokens = {
-    kind: string,
-    value: string
-}
+type Tokens = string
 
 export default function paser(code: string): Tokens[]{
     let tokens: Tokens[] = [];
     let char: string = "";
     let index: number = 0;
+    const skipChar = new Set([" ", "\n", "\t", "\r",]);
+
 
     while(code.length > index){
         let currentToken = code[index];
+        
+        if(!skipChar.has(currentToken)){
+            char += currentToken;
 
-        console.log(currentToken)
+        }else if(char !== ""){
+            tokens.push(char)
+            char = ""
+
+        }
 
         index++
     }
-
+    if (char !== "") {
+        tokens.push(char);
+    }
+    console.log(tokens)
 
 
     return tokens
