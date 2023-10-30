164002f916ea188e140480289a35aa7b6fa41bf3
leonardo <leonardocasa790@gmail.com>
2023-10-30 20:05:23 -0300

    chore: let recognition

--- lexer-study001/src/processing/process.ts
+++ lexer-study001/src/processing/process.ts
@@ -9,15 +9,17 @@ export default function parse(code) {
     let index = 0;
 
     const source = code.split(" ");
-    source.push("eof");
+    tokens.push({type: "EOF", value: "EOF"});
 
     while (source.length > index) {
         let currentToken = source[index];
-
+        console.log(currentToken)
         if (currentToken === "let") {
             tokens.push({ type: "KEYWORD", value: "let" });
         }
-
+        // else if (currentToken.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/)) {
+        //     tokens.push({ type: "IDENTIFIER", value: currentToken });
+        // }
         index++;
     }
     console.log(tokens);
