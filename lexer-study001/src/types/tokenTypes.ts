376730ff7174d24cfbc047d39e8166c92852d227
leonardo <leonardocasa790@gmail.com>
2023-11-01 18:17:18 -0300

    chore: create types folder

---
+++ lexer-study001/src/types/tokenTypes.ts
@@ -0,0 +1,20 @@
+
+export enum tokenKind{
+    LET = 'let', // Keyword let
+    EQ = 'eq', // Equality operator '=='
+    LP = 'lp', // Left parenthesis '('
+    RP = 'rp', // Right parenthesis ')'
+    LEFT_BRACE = 'lcb', // Left curly brace '{'
+    RIGHT_BRACE = 'rcb', // Right curly brace '}'
+    SC = 'sc', // Semicolon ';'
+    ADD = 'add', // Plus '+'
+    INT = 'int', // Integer literal
+    STR = 'str', // String literal
+    ID = 'id',   // Identifier
+    EOF = "EOF"
+};
+
+export type Token = {
+    kind: tokenKind,
+    value: string
+};
